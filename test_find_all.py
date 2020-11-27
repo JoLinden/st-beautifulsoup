cd0c82387f2355f1be8a52d8eb65ad419785c1c3
Jonathan Lindén <jonathan@jlinden.se>
2020-11-27 15:53:02 +0100

    Finished tests for find_all.

--- test_find_all.py
+++ test_find_all.py
@@ -3,13 +3,15 @@ import re
 import unittest
 
 
-class TestFindAllWithName(unittest.TestCase):
+class BaseTestFindAll(unittest.TestCase):
     def setUp(self):
         self.soup = BeautifulSoup('''<head><title>First element</title></head>
-                    <div class="second">Second element</div>
-                    <header>Other element</header>
-                    <div>Last element</div>''', 'html.parser')
+            <div class="second">Second element</div>
+            <header class="blue" id="main_header">Other element</header>
+            <div>Last element</div>''', 'html.parser')
 
+
+class TestFindAllWithName(BaseTestFindAll):
     def test_exactly_one_element(self):
         soup = BeautifulSoup('<div>First element</div>', 'html.parser')
         result = soup.find_all('div')
@@ -58,7 +60,7 @@ class TestFindAllWithName(unittest.TestCase):
 
         self.assertListEqual(
             ['<head><title>First element</title></head>',
-             '<header>Other element</header>'],
+             '<header class="blue" id="main_header">Other element</header>'],
             list(map(str, result)))
 
     def test_match_tags_with_list(self):
@@ -66,7 +68,7 @@ class TestFindAllWithName(unittest.TestCase):
 
         self.assertListEqual(
             ['<head><title>First element</title></head>',
-             '<header>Other element</header>'],
+             '<header class="blue" id="main_header">Other element</header>'],
             list(map(str, result)))
 
     def test_match_tags_with_function(self):
@@ -76,7 +78,8 @@ class TestFindAllWithName(unittest.TestCase):
         result = self.soup.find_all(match_name)
 
         self.assertListEqual(
-            ['<div class="second">Second element</div>'],
+            ['<div class="second">Second element</div>',
+             '<header class="blue" id="main_header">Other element</header>'],
             list(map(str, result)))
 
     def test_match_all_tags(self):
@@ -86,26 +89,12 @@ class TestFindAllWithName(unittest.TestCase):
             ['<head><title>First element</title></head>',
              '<title>First element</title>',
              '<div class="second">Second element</div>',
-             '<header>Other element</header>',
+             '<header class="blue" id="main_header">Other element</header>',
              '<div>Last element</div>'],
             list(map(str, result)))
 
 
-class FindAllWithKeywordArgs(unittest.TestCase):
-    def setUp(self):
-        self.soup = BeautifulSoup('''<head><title>First element</title></head>
-            <div class="second">Second element</div>
-            <header class="blue" id="main_header">Other element</header>
-            <div>Last element</div>''', 'html.parser')
-
-    def test_tags_containing_string(self):
-        result = self.soup.find_all(string=re.compile(r'st\s'))
-
-        self.assertListEqual(
-            ['First element',
-             'Last element'],
-            list(map(str, result)))
-
+class TestFindAllWithKeywordArgs(BaseTestFindAll):
     def test_keyword_attributes(self):
         result = self.soup.find_all(id="main_header")
 
@@ -120,6 +109,130 @@ class FindAllWithKeywordArgs(unittest.TestCase):
             ['<div class="second">Second element</div>',
              '<header class="blue" id="main_header">Other element</header>'],
             list(map(str, result)))
+
+    def test_tags_without_a_keyword_attribute(self):
+        result = self.soup.find_all(class_=False)
+
+        self.assertListEqual(
+            ['<head><title>First element</title></head>',
+             '<title>First element</title>',
+             '<div>Last element</div>'],
+            list(map(str, result)))
+
+    def test_tags_with_multiple_keyword_attributes(self):
+        result = self.soup.find_all(class_="blue", id="main_header")
+
+        self.assertListEqual(
+            ['<header class="blue" id="main_header">Other element</header>'],
+            list(map(str, result)))
+
+    def test_attrs_keyword_argument(self):
+        result = self.soup.find_all(attrs={'class': "blue"})
+
+        self.assertListEqual(
+            ['<header class="blue" id="main_header">Other element</header>'],
+            list(map(str, result)))
+
+    def test_exact_class_names(self):
+        soup = BeautifulSoup('''<div class="one two"></div>
+        <span class="two one"></span>
+        ''', 'html.parser')
+        result = soup.find_all(attrs={'class': "one two"})
+
+        self.assertListEqual(
+            ['<div class="one two"></div>'],
+            list(map(str, result)))
+
+
+class TestFindAllWithStringArgument(BaseTestFindAll):
+    def test_match_single_string(self):
+        result = self.soup.find_all(string="Second element")
+
+        self.assertListEqual(
+            ['Second element'],
+            list(map(str, result)))
+
+    def test_match_multiple_strings(self):
+        result = self.soup.find_all(string=['First element', 'Second element'])
+
+        self.assertListEqual(
+            ['First element', 'Second element'],
+            list(map(str, result)))
+
+    def test_match_strings_by_regex(self):
+        result = self.soup.find_all(string=re.compile(r'st'))
+
+        self.assertListEqual(
+            ['First element', 'Last element'],
+            list(map(str, result)))
+
+    def test_match_strings_by_function(self):
+        def match_function(s):
+            return len(s) > 2 and s[1] in ['a', 'e']
+
+        result = self.soup.find_all(string=match_function)
+
+        self.assertListEqual(
+            ['Second element', 'Last element'],
+            list(map(str, result)))
+
+    def test_find_all_strings(self):
+        result = self.soup.find_all(string=True)
+
+        self.assertListEqual(
+            ['First element', '\n', 'Second element', '\n',
+             'Other element', '\n', 'Last element'],
+            list(map(str, result)))
+
+    def test_match_tags_containing_string(self):
+        self.soup.append(self.soup.new_tag('div'))
+        result = self.soup.find_all(True, string=True)
+
+        self.assertListEqual(
+            ['<head><title>First element</title></head>',
+             '<title>First element</title>',
+             '<div class="second">Second element</div>',
+             '<header class="blue" id="main_header">Other element</header>',
+             '<div>Last element</div>'],
+            list(map(str, result)))
+
+
+class TestFindAllWithLimit(BaseTestFindAll):
+    def test_limit_number_of_matches(self):
+        result = self.soup.find_all(limit=2)
+
+        self.assertEqual(len(result), 2)
+
+    def test_limit_number_of_matches_for_specific_tag_name(self):
+        result = self.soup.find_all('div', limit=1)
+
+        self.assertListEqual(
+            ['<div class="second">Second element</div>'],
+            list(map(str, result)))
+
+    def test_high_limit_number_of_matches_for_specific_tag_name(self):
+        result = self.soup.find_all('div', limit=10)
+
+        self.assertListEqual(
+            ['<div class="second">Second element</div>',
+             '<div>Last element</div>'],
+            list(map(str, result)))
+
+
+class TestFindAllWithRecursive(BaseTestFindAll):
+    def test_child_element_with_no_recursive(self):
+        result = self.soup.find_all('title', recursive=False)
+
+        self.assertListEqual([], list(map(str, result)))
+
+    def test_find_all_top_elements(self):
+        result = self.soup.find_all(recursive=False)
+
+        self.assertListEqual([
+            '<head><title>First element</title></head>',
+            '<div class="second">Second element</div>',
+            '<header class="blue" id="main_header">Other element</header>',
+            '<div>Last element</div>'], list(map(str, result)))
 
 
 if __name__ == '__main__':
