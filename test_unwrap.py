a7cf91d129abd63bea54be8b6324856c46ea1f26
Erik Granberg <erik.granberg.9671@student.uu.se>
2020-12-01 13:00:51 +0000

    Some test for unwrap

---
+++ test_unwrap.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import unittest
+"""Tag.unwrap() is the opposite of wrap().
+It replaces a tag with whatever’s inside that tag.
+It’s good for stripping out markup"""
+
+class TestUnwrap(unittest.TestCase):
+    def setUp(self):
+        self.soup = BeautifulSoup('''<head><title>First element</title></head>
+                    <div class='second'>Second element</div>
+                    <header>Other element</header>
+                    <div>Last element</div>''', 'html.parser')
+
+    def tearDown(self):
+        pass
+
+
+    def test_unwarp_outer(self):
+        replaced = self.soup.head.unwrap()
+        self.assertEqual(str(replaced), "<head></head>")
+        with self.assertRaises(AttributeError):
+            self.soup.head.unwrap() #Can't unwrap twice
+
+    def test_unwrap_inner(self):
+        head_tag = self.soup.head
+        unwrapped = head_tag.title.unwrap()
+        self.assertEqual(str(unwrapped), "<title></title>")
+        self.assertEqual(str(head_tag),"<head>First element</head>")
+
+    def test_unwrap_nonexistant(self):
+        with self.assertRaises(AttributeError):
+            self.soup.p.unwrap()
+        with self.assertRaises(AttributeError):
+            self.soup.a.unwrap()
+
+
+    def test_unwrap_single(self):
+        unwrapped = self.soup.header.unwrap()
+        self.assertEqual(str(unwrapped),"<header></header>")
+
+    def test_unwrap_duplicate_tags(self):
+        unwrapped = self.soup.div.unwrap()
+        self.assertEqual(str(unwrapped),'<div class="second"></div>')
+        unwrapped = self.soup.div.unwrap()
+        self.assertEqual(str(unwrapped),'<div></div>')
+
+
+    def test_unwrap_all(self):
+        self.soup.head.unwrap()
+        self.soup.title.unwrap()
+        self.soup.div.unwrap()
+        self.soup.header.unwrap()
+        self.soup.div.unwrap()
+        self.assertEqual(str(self.soup), "First element\nSecond element\nOther element\nLast element")
+if __name__ == '__main__':
+    unittest.main()
