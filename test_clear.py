2172a6b2aa90a80e1b60c59bf065d5c69f9f9bf4
Florin Perpelea <perpelea.f@gmail.com>
2020-12-08 11:18:54 +0100

    Commented tests

--- test_clear.py
+++ test_clear.py
@@ -4,24 +4,19 @@ import unittest
 
 class testClear(unittest.TestCase):
     
+    # This test will check a simple clear call and we want to see if it clears as specified in the documentation
     def test_simple_clear(self):
         soup = bs("<a href=test>TEST TEST<i>test</i></a>", 'html.parser')
         tag = soup.a
         tag.clear()
         self.assertEqual(str(tag), '<a href="test"></a>')
 
+    # This test checks if we give clear an empty tag if he returns an empty tag
     def test_clear_empty(self):
-        soup = bs("<a>TEST TEST<i>test</i></a>", 'html.parser')
-        tag = soup.a
-        tag.clear()
-        self.assertEqual(str(tag), '<a></a>')
-
-    def test_clear_no_tag(self):
         soup = bs("<a></a>", 'html.parser')
         tag = soup.a
         tag.clear()
         self.assertEqual(str(tag), '<a></a>')
-
 
 if __name__ == '__main__':
     unittest.main()
--- test_extend.py
+++ test_extend.py
@@ -4,16 +4,19 @@ import unittest
 
 class testExtend(unittest.TestCase):
     
+    # This test checks if the extend funtion works as intender and adds the given inputs to the tag that we already have
     def test_simple_extend(self):
         soup = bs("<a>TEST TEST</a>", 'html.parser')
         soup.a.extend([" ", "extended"])
         self.assertEqual(str(soup),"<a>TEST TEST extended</a>")
     
+    # This test checks if we have no input if it adds or not anything to the tag that we are trying to extend
     def test_extend_nothing_passed(self):
         soup = bs("<a>Test</a>", 'html.parser')
         soup.a.extend([])
         self.assertEqual(str(soup),"<a>Test</a>")
 
+    # This test checks if the content of a tag is what suppose to be based on the documentation
     def test_contents(self):
         soup = bs("<a>TEST TEST</a>", 'html.parser')
         soup.a.extend(["extend", " "])
